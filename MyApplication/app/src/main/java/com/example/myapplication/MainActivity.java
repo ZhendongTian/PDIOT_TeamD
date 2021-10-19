d2146e6f1fdc066b3ac38c7cc1076b662c1d279c
Jerry <jerryzhao173985@gmail.com>
2021-10-19 12:52:29 +0100

    update app detect button for prediction with trained model

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -51,29 +52,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                float prediction = inference(input.getText().toString());
-                float prediction = inference();
-                output.setText(Float.toString(prediction));
+                Object[] results = inference();
+
+                int class_index = (Integer) results[0];
+                float confidence = (Float) results[1];
+                Log.d("myTag", Integer.toString(class_index) + " , "+ Float.toString(confidence));
+
+                String[] motion_list = {"Standing",
+                        "Sitting bent forward",
+                        "Walking at normal speed"};
+                String motion = motion_list[class_index];
+
+                output.setText(motion+", with confidence: "+Float.toString(confidence)); // Integer.toString(prediction)
             }
         });
 
     }
 
-    public float inference(){    //String s
-        Float[][][] inputValue = new Float[1][50][6];
+    public Object[] inference(){    //String s
+        float[][][] inputValue = new float[1][50][6];
         for(int i=0; i<50; i++){
             for(int j=0; j<6; j++){
                 inputValue[0][i][j] = (float) 0;
             }
         }
 //        inputValue[0] = Float.valueOf(s);
+        Log.d("myTag", Float.toString(inputValue[0][49][5]));
 
-        Float[] outputValue = new Float[1];
+        float[][] outputValue = new float[1][3];
         tflite.run(inputValue, outputValue);
-        float inferedValue = outputValue[0];
-        return inferedValue;
+        float inferedValue = outputValue[0][0];
+        Log.d("myTag", Float.toString(outputValue[0][0]));
+//        int index = argmax(outputValue[0]);
+        //Obtained highest prediction
+        Object[] results = argmax(outputValue[0]);
+
+        return results;
     }
 
-
+    public static Object[] argmax(float[] array){
+        int best = -1;
+        float best_confidence = 0.0f;
+        for(int i = 0;i < array.length;i++){
+            float value = array[i];
+            if (value > best_confidence){
+                best_confidence = value;
+                best = i;
+            }
+        }
+        return new Object[]{best,best_confidence};
+    }
 
     private MappedByteBuffer loadModelFile() throws IOException {
         AssetFileDescriptor fileDescriptor = this.getAssets().openFd("cnn.tflite");
